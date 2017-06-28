308f9b44eb0a1398f7003e97837c8ae113626b8f
Ubuntu <ubuntu@ip-172-31-11-53.ap-southeast-1.compute.internal>
2017-06-28 04:41:13 +0000

    Refactored Handler

--- app/Handlers/PortfolioHandler.php
+++ app/Handlers/PortfolioHandler.php
@@ -10,52 +10,41 @@ use Illuminate\Support\Facades\Log;
 
 class PortfolioHandler {
 	
-
 	public function __construct(){
 		$this->config = include('../config/communication.php');
-		$this->client = new Client();
-		$this->headers = array(
+		$this->headers = [
 				'Content-Type'  => 'application/json',
-			 	'Authorization' => 'Bearer '  . $this->config['bearer_token'],
+			 	'Authorization' => 'Bearer '.$this->config['bearer_token'],
 				'Referer'       => 'test-server'	
-		);
+		];
+		$this->client = new Client(['base_uri' => $this->config['base_url'], 'headers' => $this->headers]);
 	}
 	
 
 
 	public function RequestData($client_id){
-		$response = $this->client->request('GET', $this->config['current_url'] . $client_id, [
-			'headers' => $this->headers 
-		]) ;
-		
+		$query = ['clientid' => $client_id];
+		$response = $this->client->request('GET', 'current', ['query' => $query]);
 		return $response->getBody();
 	}
 
 	
-	// TODO: it should be possible to change it to put the parameters into an array, without creating such long ]
-	// query string.
         public function RequestHistoricalData($client_id, $days) {
-            $response = $this->client->request('GET', $this->config['historical_url'] . $client_id . "&days=" . $days, [
-                'headers' => $this->headers 
-      		]);
-            	
+		$query = ['clientid' => $client_id, 'days' => $days];
+		$response = $this->client->request('GET', 'history', ['query' => $query]);
 		return $response->getBody();
         }
 
 	public function RequestAccountData($client_id) {
-		$response = $this->client->request('GET', $this->config['account_info_url'] . $client_id, [
-			'headers' => $this->headers 
-		]);
-
+		$query = ['clientid' => $client_id];
+		$response = $this->client->request('GET', 'account', ['query' => $query]);
 		return $response->getBody();
 	}
 
 
 	public function getMarketShare($client_id) {
-		$response = $this->client->request('GET', $this->config['market_share_url'] . $client_id, [
-			'headers' => $this->headers
-		]);
-
+		$query = ['clientid' => $client_id];
+		$response = $this->client->request('GET', 'marketshare', ['query' => $query]);
 		return $response->getBody();	
 	}
 
